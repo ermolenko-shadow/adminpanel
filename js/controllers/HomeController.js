1ad4c2a511f277ec0a5b60319b7c45939a084210
ermolenko-shadow <isupergeneral@gmail.com>
2017-01-28 20:19:00 +0300

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,7 +1,7 @@
 app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
   $scope.message = "HomeController";
 
-  $http.get("http://worker.16mb.com/api/get_all_order.php")
+  $http.get("https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json")
     .then(function(response) {
         $scope.orders = response.data;
     });
